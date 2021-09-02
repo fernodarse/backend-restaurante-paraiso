f769afc918fc1543d48027ae7b4dbf9e4830027b
fernodarse <fernandonodarsegarcia@gmail.com>
2021-09-02 17:23:14 -0400

    auto

--- dist/encryption/encryption.service.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class EncryptionService {
-    private saltOrRounds;
-    constructor();
-    hash(plain: string): Promise<string>;
-    compare(plain: string, encrypted: string): Promise<boolean>;
-}

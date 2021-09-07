7aa7de5909c20e5f1a6a7f0af38f2f4343af0182
fernodarse <fernandonodarsegarcia@gmail.com>
2021-09-07 11:32:56 -0400

    Aumento del tiempo de exiprar la session de user

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { JwtStrategy } from './jwt.strategy';
   imports: [UsersModule, PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '3600s' },
     })
     ,],
   providers: [AuthService, LocalStrategy, JwtStrategy],

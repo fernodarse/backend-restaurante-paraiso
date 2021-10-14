f375549440830cf77034a77e17e2b7bab0a81921
fernodarse <fernandonodarsegarcia@gmail.com>
2021-10-14 14:59:00 -0400

    build despues de contrasena x aplicaciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { MailModule } from './mail/mail.module';
 
 @Module({
   imports: [             //'mongodb://admin:admin123@localhost/restaurante-paraiso' //mongodb+srv://admin:admin123@cluster0.2atrz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-    MongooseModule.forRoot("mongodb+srv://admin:admin123@cluster0.2atrz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"),
+    MongooseModule.forRoot("mongodb://admin:admin123@localhost/restaurante-paraiso"),
     MulterModule.register({ dest: './files', }),
     MenuModule,
     EventoModule,

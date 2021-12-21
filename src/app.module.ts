0d812856a7c32655744e14195b0291f5d118d1b5
fernodarse <fernandonodarsegarcia@gmail.com>
2021-12-21 07:57:35 -0500

    comit prueba

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { BookingModule } from './booking/booking.module';
 import { MailModule } from './mail/mail.module';
 
 @Module({
-  imports: [             //'mongodb://admin:admin123@localhost/restaurante-paraiso' //mongodb+srv://admin:admin123@cluster0.2atrz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority , "mongodb://ef90314ed7b34e99a590c77280f73690:admin123@11a.mongo.evennode.com:27018,11b.mongo.evennode.com:27018/ef90314ed7b34e99a590c77280f73690?replicaSet=eu-11"
+  imports: [             // 'mongodb://admin:admin123@localhost/restaurante-paraiso' //mongodb+srv://admin:admin123@cluster0.2atrz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority , "mongodb://ef90314ed7b34e99a590c77280f73690:admin123@11a.mongo.evennode.com:27018,11b.mongo.evennode.com:27018/ef90314ed7b34e99a590c77280f73690?replicaSet=eu-11"
     MongooseModule.forRoot("mongodb+srv://admin:admin123@cluster0.2atrz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"),
     MulterModule.register({ dest: './files', }),
     MenuModule,
